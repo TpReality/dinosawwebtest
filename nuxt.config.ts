19c54d125b5331ae4059c80f4039e21a4e5e8d06
TpReality <416410458@qq.com>
2025-10-29 16:16:42 +0800

    增加多语言

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -64,7 +64,7 @@ export default defineNuxtConfig({
 
   nitro: {
     prerender: {
-      concurrency: 5,
+      concurrency: 3,
       crawlLinks: false,
       failOnError: false,
       routes: (() => {
